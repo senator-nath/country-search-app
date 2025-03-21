bdee4690332c571cefb985ace4f159dd09d8cdad
senator-nath <nathanielosana18@gmail.com>
2025-03-21 09:43:58 +0100

    Updated the Ui while spinning

--- src/CountryDetail.jsx
+++ src/CountryDetail.jsx
@@ -44,7 +44,7 @@ const CountryDetail = () => {
 
   // Spinner component
   const Spinner = () => (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className={`flex justify-center items-center min-h-screen dark:bg-gray-900`}>
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900 dark:border-gray-100"></div>
     </div>
   );
